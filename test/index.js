b9da79cf59fb2632ef2095b0dd480a2fa0acbef2
Darshak Parikh <darshak@openmailbox.org>
2016-01-18 07:04:06 +0000

    Refactor tests
    - One less level of nesting
    - Within `before`, `done()` is now called when helper run ends
    - Expected test values are now hardcoded

--- test/index.js
+++ test/index.js
@@ -4,51 +4,47 @@ const Lab = require('lab');
 const lab = exports.lab = Lab.script();
 const path = require('path');
 
-lab.experiment('generator-awesome-list', () => {
-	const props = {
-		title: 'Awesome Awesomeness',
-		description: 'A curated list of awesomeness',
-		username: 'Awesomov Awesomovic',
-		email: 'awe@so.me'
-	};
+lab.before((done) => {
+	helpers.run(path.join(__dirname, '../app'))
+		.withPrompts({
+			title: 'Awesome Awesomeness',
+			description: 'A curated list of awesomeness',
+			username: 'Awesomov Awesomovic',
+			email: 'awe@so.me'
+		})
+		.on('end', done);
+});
 
-	lab.before((done) => {
-		helpers.run(path.join(__dirname, '../app'))
-			.withPrompts(props);
+lab.test('all files are created', (done) => {
+	assert.file([
+		'.gitattributes',
+		'code-of-conduct.md',
+		'contributing.md',
+		'readme.md'
+	]);
+	done();
+});
+
+lab.experiment('code-of-conduct.md', () => {
+	lab.test('contains email', (done) => {
+		assert.fileContent('code-of-conduct.md', 'awe@so.me');
+		done();
+	});
+});
+
+lab.experiment('readme.md', () => {
+	lab.test('contains title', (done) => {
+		assert.fileContent('readme.md', 'Awesome Awesomeness');
 		done();
 	});
 
-	lab.test('all files are created', (done) => {
-		assert.file([
-			'.gitattributes',
-			'code-of-conduct.md',
-			'contributing.md',
-			'readme.md'
-		]);
+	lab.test('contains description', (done) => {
+		assert.fileContent('readme.md', 'A curated list of awesomeness');
 		done();
 	});
 
-	lab.experiment('code-of-conduct.md', () => {
-		lab.test('contains email', (done) => {
-			assert.fileContent('code-of-conduct.md', props.email);
-			done();
-		});
-	});
-
-	lab.experiment('readme.md', () => {
-		lab.test('contains title', (done) => {
-			assert.fileContent('readme.md', props.title);
-			done();
-		});
-
-		lab.test('contains description', (done) => {
-			assert.fileContent('readme.md', props.description);
-			done();
-		});
-
-		lab.test('contains username', (done) => {
-			assert.fileContent('readme.md', props.username);
-			done();
-		});
+	lab.test('contains username', (done) => {
+		assert.fileContent('readme.md', 'Awesomov Awesomovic');
+		done();
 	});
 });
