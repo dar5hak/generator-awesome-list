ea5e5ad5158421fb4c335e55f0953db4e6253522
ayatullahshop-create <ayatullahshop@gmail.com>
2025-12-12 11:57:57 +0600

    Update config.py

--- config.py
+++ config.py
@@ -3,5 +3,5 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-BOT_TOKEN = os.getenv("BOT_TOKEN")
-ADMIN_ID = int(os.getenv("ADMIN_ID"))
+BOT_TOKEN = os.getenv("8182617462:AAF4muztgV1kAAnYY0d27WqGaruupe2-KGE")
+ADMIN_ID = int(os.getenv("7779433887"))
